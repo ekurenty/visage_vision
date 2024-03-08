1b99e30a20bb4d5594cb440749dc99c3e1c2ccd3
Edinam Kurenty <eakurenty@gmail.com>
2024-03-08 18:28:54 +1100

    Added working streamlit feed

---
+++ streamlit_feed/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+# Local Modules
+import functions
+
+# Setting page layout
+st.set_page_config(
+    page_title="Facial Expression Detection using YOLOv8",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Main page heading
+st.title("Facial Expression Detection using YOLOv8")
+
+# Sidebar
+st.sidebar.header("Confidence Level")
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 25, 100, 40)) / 100
+
+model_path = 'best.pt'
+
+# Load the model
+try:
+    model = functions.load_model(model_path)
+except Exception as ex:
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
+
+functions.play_webcam(confidence, model)
---
+++ streamlit_feed/functions.py
@@ -0,0 +1,64 @@
+
+from ultralytics import YOLO
+import cv2
+import streamlit as st
+import numpy as np
+
+
+def load_model(model_path):
+    model = YOLO(model_path)
+    return model
+
+
+def _display_detected_frames(conf, model, st_frame, image):
+    """
+    Display the detected objects on a video frame using the YOLOv8 model.
+    Args:
+    - conf (float): Confidence threshold for object detection.
+    - model (YoloV8): A trained YOLOv8 object detection model.
+    - st_frame (Streamlit object): A Streamlit object to display the detected video.
+    - image (numpy array): A numpy array representing the video frame.
+    """
+
+    # Resize the image to a standard size
+    image = cv2.resize(image, (720, int(720*(9/16))))
+
+    # PASS TO API
+    # Predict the objects in the image using the YOLOv8 model
+    res = model.predict(image, conf=conf)
+
+    # Plot the detected objects on the video frame
+    res_plotted = res[0].plot()
+    st_frame.image(res_plotted,
+                   caption='Detected Video',
+                   channels="BGR",
+                   use_column_width=True
+                   )
+
+
+
+def play_webcam(conf, model):
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+    """
+    source_webcam = 0
+    if st.sidebar.button('Start'):
+        try:
+            vid_cap = cv2.VideoCapture(source_webcam)
+            st_frame = st.empty()
+            while (vid_cap.isOpened()):
+                success, image = vid_cap.read()
+                if success:
+                    _display_detected_frames(conf,
+                                             model,
+                                             st_frame,
+                                             image,
+                                             )
+                else:
+                    vid_cap.release()
+                    break
+        except Exception as e:
+            st.sidebar.error("Error loading video: " + str(e))
